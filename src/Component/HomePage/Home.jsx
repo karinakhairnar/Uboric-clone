c51972d9a38aad0c626e6aa5d6fb329c5abf5d69
Saraswati121 <99686266+Saraswati121@users.noreply.github.com>
2022-05-05 14:17:45 +0530

    cntn

--- src/Component/HomePage/Home.jsx
+++ src/Component/HomePage/Home.jsx
@@ -4,6 +4,11 @@ import './home.css'
 export const Home = () => {
   return (
     <div>
+      <div className='main'>
+        <img src="https://www.uboric.com/wp-content/uploads/2021/11/grocery.jpg" alt="" />
+        <img src="https://www.uboric.com/wp-content/uploads/2021/11/health-care.jpg" alt="" />
+        <img src="https://www.uboric.com/wp-content/uploads/2021/11/clothing.jpg" alt="" />
+      </div>
       <div className='first'>
       <img src="https://www.uboric.com/wp-content/uploads/elementor/thumbs/kitchen-ware-heading-pkx8tvxfmbbsdlrzrk3iwlbxruc7cn5sq339xkx9cg.png"/>
       <h1 >KITCHEN WARE</h1>
@@ -48,6 +53,55 @@ export const Home = () => {
       <img src="https://www.uboric.com/wp-content/uploads/2020/12/Braclate-400x601.jpg" alt="" />
       <img src="https://www.uboric.com/wp-content/uploads/2020/12/Grocery-400x601.jpg" alt="" />
       <img src="https://www.uboric.com/wp-content/uploads/2020/12/Health-Care-400x601.jpg" alt="" />
+    </div>
+
+    <div className='five'>
+        <img src="https://www.uboric.com/wp-content/uploads/elementor/thumbs/spices-heading-2-e1608267173425-pkx8te2i0gnc90hxnudm37u6his8ae6wbmp1tbnqmo.png" alt="" />
+        <h1>SPICES</h1>
+        <img src="https://www.uboric.com/wp-content/uploads/elementor/thumbs/spices-heading-2-e1608267173425-pkx8te2i0gnc90hxnudm37u6his8ae6wbmp1tbnqmo.png" alt="" />
+    </div>
+
+    <div className='frth'>
+        <img src="https://www.uboric.com/wp-content/uploads/2020/12/Spices-1-400x601.jpg" alt="" />
+        <img src="https://www.uboric.com/wp-content/uploads/2020/12/Spices-2-400x601.jpg" alt="" />
+        <img src="https://www.uboric.com/wp-content/uploads/2020/12/Spices-3-400x601.jpg" alt="" />
+        <img src="https://www.uboric.com/wp-content/uploads/2020/12/Spices-5-400x601.jpg" alt="" />
+        <img src="https://www.uboric.com/wp-content/uploads/2020/12/Spices-4-400x601.jpg" alt="" />
+    </div>
+
+    <div className='five'>
+        <img src="https://www.uboric.com/wp-content/uploads/elementor/thumbs/shoes-icon-right-e1605175492472-pkxdahmpc96i32hqyh9qfkc7owqxivqr4zuwukso7k.png" alt="" />
+        <h1>FOOT WEAR</h1>
+        <img src="https://www.uboric.com/wp-content/uploads/elementor/thumbs/shoes-icon-left-e1605175515419-pkxdaq391ri2zk5gl2xdk07d1dl8g5oc65qa62g4nk.png" alt="" />
+    </div>
+
+    <div style={{
+      marginTop:"25px"
+    }}>
+        <img src="https://www.uboric.com/wp-content/uploads/elementor/thumbs/men-fashion-pf2rgkwglk7z47uh031vpxcshl64adc389ivxapmss.jpg" alt="" />
+    </div>
+
+    <div className='frth'>
+        <img src="https://www.uboric.com/wp-content/uploads/2020/12/Foot-Wear-1-400x601.jpg" alt="" />
+        <img src="https://www.uboric.com/wp-content/uploads/2020/12/Foot-Wear-3-400x601.jpg" alt="" />
+        <img src="https://www.uboric.com/wp-content/uploads/2020/12/Foot-Wear-2-400x601.jpg" alt="" />
+        <img src="https://www.uboric.com/wp-content/uploads/2020/12/Foot-Wear-400x601.jpg" alt="" />
+        <img src="https://www.uboric.com/wp-content/uploads/2020/12/Foot-Wear-5-400x601.jpg" alt="" />
+    </div>
+
+    <div style={{marginTop:"70px"}}>
+        <h1>The Ultimate Domestic Online Shopping Experience <br /> Platform in India</h1><br />
+    </div>
+
+    <div style={{width:"80%",margin:"auto ", textAlign:"left", lineHeight:"30px"}}>
+    <p>Online shopping with Uboric is quick, convenient and trouble-free. You can shop for the desired product right from the comfort of your home and get them delivered straight to your doorstep. Uboric offers you the chance to choose top branded products sitting in the comfort of your homes and just clicking on your requirements to get it delivered at your doorstep. Your search for the latest trending variety of unique products ends right here. A wide range of international products from global brands are available at your fingertips. We provide you with a world-class online shopping experience, along with superior service, to suit all your specific requirements. Our products are very reasonably priced and are not easily available elsewhere. Our high-end technology-based systems, combined with a human approach, ensure that you have an amazing and blissful online shopping experience with us. Our emphasis on customer delight drives every activity we undertake to provide you an ultimate, hassle-free and pleasant shopping experience. Our diverse categories of products comprise of fashion & jewellery, mobiles and tablets, home and furniture, electronics, health care and supplements, sports and fitness, beauty and perfumes. We offer almost every variety of product that your heart desires. Just Go Ahead and Explore the magical world of online shopping with Uboric! 24×7 Customer Care: For all your queries and concerns regarding your shopping orders.</p>
+    </div>
+
+    <div style={{marginTop:"30px"}}>
+      <h1>Benefits of shopping on Uboric :</h1>
+    </div>
+    <div>
+
     </div>
     </div>
   )
